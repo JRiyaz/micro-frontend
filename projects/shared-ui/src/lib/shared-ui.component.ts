c4ed666e4fa328bcc7b2ee4d372c3d0921b4e24c
Riyaz J <jriyaz@zeomega.com>
2024-12-16 22:06:45 +0530

    Docker working

--- projects/shared-ui/src/lib/shared-ui.component.ts
+++ projects/shared-ui/src/lib/shared-ui.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'lib-shared-ui',
     imports: [],
+    standalone: true,
     template: `
     <p>
       shared-ui works!
--- projects/shared-ui/src/lib/shared-ui.service.ts
+++ projects/shared-ui/src/lib/shared-ui.service.ts
@@ -5,7 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class SharedUiService {
   var!: number
-  constructor() { }
 
   setVar(value: number): void {
     this.var = value;
