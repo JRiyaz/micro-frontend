2b605d920a2be91486f4964207fee2b385fd8b55
Riyaz J <j.riyazu@gmail.com>
2026-04-30 01:54:24 +0530

    Include Inventory hub in sidebar

--- projects/ui-shared/src/lib/components/sidebar/sidebar.component.ts
+++ projects/ui-shared/src/lib/components/sidebar/sidebar.component.ts
@@ -252,13 +252,13 @@ export class SidebarComponent {
     },
     {
       label: 'Products',
-      route: '/dashboard/products',
+      route: '/inventory/products',
       exact: false,
       icon: this.sanitizer.bypassSecurityTrustHtml('<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 16V8a2 2 0 00-1-1.73l-7-4a2 2 0 00-2 0l-7 4A2 2 0 003 8v8a2 2 0 001 1.73l7 4a2 2 0 002 0l7-4A2 2 0 0021 16z"></path><polyline points="3.27,6.96 12,12.01 20.73,6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>')
     },
     {
       label: 'Orders',
-      route: '/dashboard/orders',
+      route: '/inventory/orders',
       exact: false,
       icon: this.sanitizer.bypassSecurityTrustHtml('<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>')
     },
